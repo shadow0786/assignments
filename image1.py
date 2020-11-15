512162991df61d6a314ee4382496c41344b9efd5
shadow0786 <74306526+shadow0786@users.noreply.github.com>
2020-11-15 03:17:26 +0000

    Update image1.py

--- image1.py
+++ image1.py
@@ -23,7 +23,7 @@ class image_converter:
     self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw",Image,self.callback1)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
-    self.cv_image1_spheres_pub = rospy.Publisher("cv_image1/spheres_pub" , Float64MultiArray, queue_length = 10)
+    self.cv_image1_data_pub = rospy.Publisher("cv_image1/data_pub" , Float64MultiArray, queue_length = 12)
 
 
   # Recieve data from camera 1, process it, and publish
@@ -33,7 +33,21 @@ class image_converter:
       self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
+
+    self.objects = Float64MultiArray()
+    green = self.find_green(self.cv_image1)
+    yellow = self.find_yellow(self.cv_image1)
+    red = self.find_red(self.cv_image1)
+    blue = self.find_blue(self.cv_image1)
+
     
+    target_S = self.locate_targets(self, cv_image1, templateS , templateR)[0]
+    target_R = self.locate_targets(self, cv_image1, templateS , templateR)[1]
+    
+    data = self.makeData(yellow , blue , green , red , target_S , target_R)
+    self.objects.data = data
+    
+  
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
 
@@ -42,8 +56,33 @@ class image_converter:
     # Publish the results
     try: 
       self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
+      self.cv_image1_data_pub.publish(self.objects)
     except CvBridgeError as e:
       print(e)
+  def makeData(self , array , array1 , array2 , array3 , array4 , array5 , array6):
+    return np.array([array[0],array[1],array1[0],array1[1],array2[0],array2[1],array3[0],array3[1],array4[0],array4[1] , array5[0],array5[1] , array6[0],array6[1]])
+      
+  def locate_target_sphere(self, image1, template):
+    matching = cv2.matchTemplate(image1, template, 0)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matching)
+    x = min_loc[0] 
+    y = min_loc[1]
+    return np.array([x,y])
+  
+  def locate_target_rectangle(self, image1, template):
+    matching = cv2.matchTemplate(image1, template, 0)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matching)
+    x = min_loc[0] 
+    y = min_loc[1]
+    return np.array([x,y])
+  
+  def locate_targets(self, image1, templateS , templateR):
+    maskOrange = self.find_orange(image1)
+    templateSphere = cv2.imread(templateS, 0)
+    templateRectangle = cv2.imread(templateR, 0)
+    targetSphere = self.locate_target_sphere(maskOrange, templateSphere)
+    targetRectangle = self.locate_target_rectangle(maskOrange, templateRectangle)
+    return np.array([targetSphere , targetRectangle])
       
   def find_yellow(self , image):
     mask = cv2.inRange(image , (0,100,100) , (0,255,255))
@@ -53,10 +92,10 @@ class image_converter:
     if Mo['m00' == 0]:
       cx = -10
       cy = -10
-      return np.array(cx,cy)
+      return np.array([cx,cy])
     cx = int(Mo['m10'] / Mo['m00'])
     cy = int(Mo['m01'] / Mo['m00'])
-    return np.array(cx,cy)
+    return np.array([cx,cy])
   
   def find_red(self , image):
     mask = cv2.inRange(image , (0,0,100) , (0,0,255))
@@ -66,10 +105,10 @@ class image_converter:
     if Mo['m00' == 0]:
       cx = -10
       cy = -10
-      return np.array(cx,cy)
+      return np.array([cx,cy])
     cx = int(Mo['m10'] / Mo['m00'])
     cy = int(Mo['m01'] / Mo['m00'])
-    return np.array(cx,cy)
+    return np.array([cx,cy])
   
   def find_green(self , image):
     mask = cv2.inRange(image , (0,100,0) , (0,255,0))
@@ -79,10 +118,10 @@ class image_converter:
     if Mo['m00' == 0]:
       cx = -10
       cy = -10
-      return np.array(cx,cy)
+      return np.array([cx,cy])
     cx = int(Mo['m10'] / Mo['m00'])
     cy = int(Mo['m01'] / Mo['m00'])
-    return np.array(cx,cy)
+    return np.array([cx,cy])
   
   def find_blue(self , image):
     mask = cv2.inRange(image , (100,0,0) , (255,0,0))
@@ -92,14 +131,15 @@ class image_converter:
     if Mo['m00' == 0]:
       cx = -10
       cy = -10
-      return np.array(cx,cy)
+      return np.array([cx,cy])
     cx = int(Mo['m10'] / Mo['m00'])
     cy = int(Mo['m01'] / Mo['m00'])
-    return np.array(cx,cy)
+    return np.array([cx,cy])
   
   def find_orange(self , image):
     mask  = cv2.inRange(image  ,(50,100,110) , (90,185,220))
     return mask
+   
     
     
               
