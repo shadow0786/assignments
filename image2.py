0d2e61fe5162985b1db40bd116bb7bfe3ba045ca
shadow0786 <74306526+shadow0786@users.noreply.github.com>
2020-11-17 14:53:13 +0000

    Update image2.py

--- image2.py
+++ image2.py
@@ -23,6 +23,25 @@ class image_converter:
     self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw",Image,self.callback2)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
+    self.yellow_img1_pos = np.array([0,0])
+    self.blue_img1_pos = np.array([0,0])
+    self.green_img1_pos = np.array([0,0])
+    self.red_img1_pos = np.array([0,0])
+    self.targetS_img1_pos = np.array([0,0])
+    self.targetR_img1_pos = np.array([0,0])
+    self.img1_object_data = rospy.Subscriber("/image1/objects", Float64MultiArray, self.callbackIMG1)
+    self.robot_Sinjoint2 = rospy.Publisher("/robot/Sinjoint2_position_controller/command", Float64, queue_size = 10)
+    self.robot_Sinjoint3 = rospy.Publisher("/robot/Sinjoint3_position_controller/command", Float64, queue_size = 10)
+    self.robot_Sinjoint4 = rospy.Publisher("/robot/Sinjoint4_position_controller/command", Float64, queue_size = 10)
+    self.time = np.array([rospy.get_time()], dtype='float64')
+    
+   def callback1(self,data):
+    circles_pos1 = np.array(data.data)
+    self.yellow_proj_pos1 =  np.array([circles_pos1[0],circles_pos1[1]])
+    self.blue_proj_pos1 = np.array([circles_pos1[2],circles_pos1[3]])
+    self.green_proj_pos1 = np.array([circles_pos1[4],circles_pos1[5]])
+    self.red_proj_pos1 = np.array([circles_pos1[6],circles_pos1[7]])
+    self.target_proj_pos1 = np.array([circles_pos1[8],circles_pos1[9]]) 
 
 
   # Recieve data, process it, and publish
