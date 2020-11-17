25510e295a3f007086d3efb0cf00588e637b8388
shadow0786 <74306526+shadow0786@users.noreply.github.com>
2020-11-17 15:11:37 +0000

    Update image2.py

--- image2.py
+++ image2.py
@@ -35,13 +35,14 @@ class image_converter:
     self.robot_Sinjoint4 = rospy.Publisher("/robot/Sinjoint4_position_controller/command", Float64, queue_size = 10)
     self.time = np.array([rospy.get_time()], dtype='float64')
     
-   def callback1(self,data):
-    circles_pos1 = np.array(data.data)
-    self.yellow_proj_pos1 =  np.array([circles_pos1[0],circles_pos1[1]])
-    self.blue_proj_pos1 = np.array([circles_pos1[2],circles_pos1[3]])
-    self.green_proj_pos1 = np.array([circles_pos1[4],circles_pos1[5]])
-    self.red_proj_pos1 = np.array([circles_pos1[6],circles_pos1[7]])
-    self.target_proj_pos1 = np.array([circles_pos1[8],circles_pos1[9]]) 
+   def callbackIMG1(self,data):
+    self.objects_coordinates = np.array(data.data)
+    self.yellow_img1_pos =  np.array([objects_coordinates[0],objects_coordinates[1]])
+    self.blue_img1_pos = np.array([objects_coordinates[2],objects_coordinates[3]])
+    self.green_img1_pos = np.array([objects_coordinates[4],objects_coordinates[5]])
+    self.red_img1_pos = np.array([objects_coordinates[6],objects_coordinates[7]])
+    self.targetS_img1_pos = np.array([objects_coordinates[8],objects_coordinates[9]])
+    self.targetR_img1_pos = np.array([objects_coordinates[10],objects_coordinates[11]]) 
 
 
   # Recieve data, process it, and publish
@@ -61,6 +62,84 @@ class image_converter:
       self.image_pub2.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
     except CvBridgeError as e:
       print(e)
+      
+  def locate_target_sphere(self, image1, template):
+    matching = cv2.matchTemplate(image1, template, 0)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matching)
+    x = min_loc[0] 
+    y = min_loc[1]
+    return np.array([x,y])
+
+  def locate_target_rectangle(self, image1, template):
+    matching = cv2.matchTemplate(image1, template, 0)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matching)
+    x = min_loc[0] 
+    y = min_loc[1]
+    return np.array([x,y])
+
+  def locate_targets(self, image1, templateS , templateR):
+    maskOrange = self.find_orange(image1)
+    templateSphere = cv2.imread(templateS, 0)
+    templateRectangle = cv2.imread(templateR, 0)
+    targetSphere = self.locate_target_sphere(maskOrange, templateSphere)
+    targetRectangle = self.locate_target_rectangle(maskOrange, templateRectangle)
+    return np.array([targetSphere , targetRectangle])
+
+  def find_yellow(self , image):
+    mask = cv2.inRange(image , (0,100,100) , (0,255,255))
+    kernel = np.ones((5,5) , np.unit8)
+    NewMask = cv2.dilate(mask , kernel , iteration = 3)
+    Mo = cv2.moments(NewMask)
+    if Mo['m00' == 0]:
+      cx = -10
+      cy = -10
+      return np.array([cx,cy])
+    cx = int(Mo['m10'] / Mo['m00'])
+    cy = int(Mo['m01'] / Mo['m00'])
+    return np.array([cx,cy])
+
+  def find_red(self , image):
+    mask = cv2.inRange(image , (0,0,100) , (0,0,255))
+    kernel = np.ones((5,5) , np.unit8)
+    NewMask = cv2.dilate(mask , kernel , iteration = 3)
+    Mo = cv2.moments(NewMask)
+    if Mo['m00' == 0]:
+      cx = -10
+      cy = -10
+      return np.array([cx,cy])
+    cx = int(Mo['m10'] / Mo['m00'])
+    cy = int(Mo['m01'] / Mo['m00'])
+    return np.array([cx,cy])
+
+  def find_green(self , image):
+    mask = cv2.inRange(image , (0,100,0) , (0,255,0))
+    kernel = np.ones((5,5) , np.unit8)
+    NewMask = cv2.dilate(mask , kernel , iteration = 3)
+    Mo = cv2.moments(NewMask)
+    if Mo['m00' == 0]:
+      cx = -10
+      cy = -10
+      return np.array([cx,cy])
+    cx = int(Mo['m10'] / Mo['m00'])
+    cy = int(Mo['m01'] / Mo['m00'])
+    return np.array([cx,cy])
+
+  def find_blue(self , image):
+    mask = cv2.inRange(image , (100,0,0) , (255,0,0))
+    kernel = np.ones((5,5) , np.unit8)
+    NewMask = cv2.dilate(mask , kernel , iteration = 3)
+    Mo = cv2.moments(NewMask)
+    if Mo['m00' == 0]:
+      cx = -10
+      cy = -10
+      return np.array([cx,cy])
+    cx = int(Mo['m10'] / Mo['m00'])
+    cy = int(Mo['m01'] / Mo['m00'])
+    return np.array([cx,cy])
+
+  def find_orange(self , image):
+    mask  = cv2.inRange(image  ,(50,100,110) , (90,185,220))
+    return mask
 
 # call the class
 def main(args):
