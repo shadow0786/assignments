6e0114b6c2948b802839c9e546ae9ce3e5018b5d
Hazem1998 <46978637+Hazem1998@users.noreply.github.com>
2020-11-20 07:19:33 +0000

    Update image2.py
    fixed brackets

--- image2.py
+++ image2.py
@@ -229,7 +229,7 @@ class image_converter:
         kernel = np.ones((5,5) , np.uint8)
         NewMask = cv2.dilate(mask , kernel , iterations = 3)
         Mo = cv2.moments(NewMask)
-        if (Mo['m00' == 0]):
+        if (Mo['m00'] == 0):
             cx = -10
             cy = -10
             return np.array([cx,cy])
@@ -242,7 +242,7 @@ class image_converter:
         kernel = np.ones((5,5) , np.uint8)
         NewMask = cv2.dilate(mask , kernel , iterations = 3)
         Mo = cv2.moments(NewMask)
-        if (Mo['m00' == 0]):
+        if (Mo['m00'] == 0):
             cx = -10
             cy = -10
             return np.array([cx,cy])
@@ -255,7 +255,7 @@ class image_converter:
         kernel = np.ones((5,5) , np.uint8)
         NewMask = cv2.dilate(mask , kernel , iterations = 3)
         Mo = cv2.moments(NewMask)
-        if (Mo['m00' == 0]):
+        if (Mo['m00'] == 0):
             cx = -10
             cy = -10
             return np.array([cx,cy])
@@ -268,7 +268,7 @@ class image_converter:
         kernel = np.ones((5,5) , np.uint8)
         NewMask = cv2.dilate(mask , kernel , iterations = 3)
         Mo = cv2.moments(NewMask)
-        if (Mo['m00' == 0]):
+        if (Mo['m00'] == 0):
             cx = -10
             cy = -10
             return np.array([cx,cy])
